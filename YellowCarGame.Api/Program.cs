c85106462d9f639e2d0eaeee3ec1d0d7da6c0a77
Nikolaj Have <nhave06@gmail.com>
2026-03-31 10:51:45 +0200

    Game update & Cors

--- YellowCarGame.Api/Program.cs
+++ YellowCarGame.Api/Program.cs
@@ -32,7 +32,20 @@ namespace YellowCarGame.Api
 
             builder.Services.AddControllers();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("AllowAll");
 
             app.MapDefaultEndpoints();
 
